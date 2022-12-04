d7f5031a887726f879e95081e19364ec7f37e624
Anthony_Chiodi <adc8wlc@gmail.com>
2022-12-04 13:59:27 -0500

    added react-redux methods and gave the application access to the store'

--- SeatMe/frontend/src/index.js
+++ SeatMe/frontend/src/index.js
@@ -1,36 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
 import "./index.css";
 import App from "./App";
 import { restoreSession } from './store/csrf';
 import { createUser, loginUser, logoutUser } from './store/usersReducer';
-
-
-window.createUser = createUser
-window.loginUser = loginUser
-window.logoutUser = logoutUser
+import configureStore from './store/index'
+import { useDispatch } from "react-redux";
 
 
 const initializeApp = () => {
+  let currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
+  let initialState = {};
+
+  if (currentUser) {
+    initialState = {
+      users: {
+        [currentUser.id]: currentUser
+      }
+    };
+  };
+
+
+  window.createUser = createUser
+  window.loginUser = loginUser
+  window.logoutUser = logoutUser
+
+  const store = configureStore(initialState);
   ReactDOM.render(
     <React.StrictMode>
-      {/* <Provider store={store}> */}
+      <Provider store={store}>
+
       <App />
-      {/* </Provider> */}
+      </Provider>
     </React.StrictMode>,
     document.getElementById("root")
     );
   };
 
-  restoreSession().then(initializeApp)
-
-let currentUser = JSON.parse(sessionStorage.getItem('currentUser'));
-let initialState = {};
-
-if (currentUser) {
-    initialState = {
-        users: {
-        [currentUser.id]: currentUser
-        }
-    };
-};
+  restoreSession().then(initializeApp());
