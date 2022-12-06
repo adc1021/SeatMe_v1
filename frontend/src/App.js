cab9d517b18acc2935d6159092b757f65801e09e
Anthony_Chiodi <adc8wlc@gmail.com>
2022-12-05 21:24:33 -0500

    Corrected alignment of navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,27 +1,28 @@
-import React from 'react';
-import { Link, Route, Switch } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage'
-import LogoutButton from './components/LogoutButton';
-import SignupForm from './components/SignUp';
+import React from "react";
+import { Link, Route, Switch } from "react-router-dom";
+import LoginFormPage from "./components/LoginFormPage";
+import LogoutButton from "./components/LogoutButton";
+import SignupForm from "./components/SignUp";
 import Navigation from "./components/Navigation";
-
 
 function App() {
   return (
-    <div id='navbar'>
-      <h1>SeatMe</h1>
-      <div id='forms'>
-      <Navigation />
-      <Switch>
-       <Route path="/login">
-          <LoginFormPage />
-        </Route>
-       <Route path="/signup">
-          <SignupForm />
-        </Route>
-      </Switch>
+    <>
+      <div id="navbar">
+        <h1>SeatMe</h1>
+        <Navigation />
       </div>
-    </div>
+      <div id="forms">
+        <Switch>
+          <Route path="/login">
+            <LoginFormPage />
+          </Route>
+          <Route path="/signup">
+            <SignupForm />
+          </Route>
+        </Switch>
+      </div>
+    </>
   );
 }
 
