46da605713790c7beccd859c4f207f80aae34894
Anthony_Chiodi <adc8wlc@gmail.com>
2022-12-05 20:53:11 -0500

    removed nesting from files and fixed my sign in feature

---
+++ app/models/user.rb
@@ -0,0 +1,64 @@
+
+class User < ApplicationRecord
+    before_validation :ensure_session_token
+    validates :first_name,
+        :last_name, :email, :session_token, :phone_number, presence: true
+    validates :first_name, length: { in: 2..30, message: 'First name is required.' }
+    validates :last_name, length: { in: 2..30, message: 'Last name is required.' }
+    validates :email,
+        length: { in: 3..255 },
+        format: { with: URI::MailTo::EMAIL_REGEXP, message: 'Email is required.' },
+        uniqueness: true
+    validates :phone_number, :session_token, uniqueness: true
+    validates :phone_number,
+    format: { with: /\A\d{3}-\d{3}-\d{4}\z/, on: :create, message: 'Phone number is required' } # added a regexp I found on stack overflow for phone number validation
+
+
+    # has_many: :reservations
+
+    # has_many: :reviews
+
+    # has_many: :saved_restaurants
+
+    # def self.find_by_credentials(email)
+    #     user = User.find_by(email: email)
+    #     if user
+    #         return user
+    #     else
+    #         nil
+    #     end
+    # end
+    def self.find_by_credentials(credential)
+
+        if credential.split("@").length > 1 
+            user = User.find_by(email: credential)
+        else
+            user = User.find_by(phone_number: credential)
+        end
+        # user = User.find_by(email: credential)
+        if user
+            return user
+        else
+            nil
+        end
+    end
+
+    def ensure_session_token
+        self.session_token ||= generate_unique_session_token
+    end
+
+    def reset_session_token!
+        self.session_token = generate_unique_session_token
+        save!
+        session_token
+    end
+
+    private
+
+    def generate_unique_session_token
+        while
+            token = SecureRandom.urlsafe_base64
+        return token unless User.exists?(session_token: token)
+        end
+    end
+end
