0e3a3b79b8b9915e00c4df9c4d5c96e06ebe8427
Anthony_Chiodi <adc8wlc@gmail.com>
2022-12-07 15:49:29 -0500

    created new restaurants table

---
+++ db/migrate/20221207171355_create_restaurants.rb
@@ -0,0 +1,19 @@
+class CreateRestaurants < ActiveRecord::Migration[7.0]
+  def change
+    create_table :restaurants do |t|
+      t.string :name, null: false
+      t.text :description, null: false
+      t.text :cuisine, null: false
+      t.string :address, null: false
+      t.integer :tables, null: false
+      t.text :menu, null: false
+      t.integer :average_rating, null: false
+      t.integer :price_point, null: false
+
+
+      t.timestamps
+    end
+
+    add_index :restaurants, :name, unique: true 
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_05_143959) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_07_171355) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.string "address"
+    t.integer "tables"
+    t.text "menu"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", null: false
