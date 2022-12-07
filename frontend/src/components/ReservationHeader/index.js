a6205eaf42db27638b8342a9bba03a3bcf20ccf3
Anthony_Chiodi <adc8wlc@gmail.com>
2022-12-07 14:56:27 -0500

    Continued styling

--- frontend/src/components/ReservationHeader/index.js
+++ frontend/src/components/ReservationHeader/index.js
@@ -10,43 +10,50 @@ const ReservationHeader = () => {
       <div>
         <h1 id="res-header-title">Find your table for any occasion</h1>
       </div>
-      <div id="date-time-party-container">
-        <div id="date-menu" className="dropdown">
-          <div id="sub-date-menu">
-            <select>
-
+      <div id="flex-box">
+        <div id="date-time-party-container">
+          <div>
+            <div>
+              <select id="time-container" className="dropdown"></select>
+            </div>
+          </div>
+          <div>
+            <select id="time-container" className="dropdown">
+              <option>11:00 AM</option>
+              <option>12:00 PM</option>
+              <option>1:00 PM</option>
+              <option>2:00 PM</option>
+              <option>3:00 PM</option>
+              <option>4:00 PM</option>
+              <option>5:00 PM</option>
+              <option>6:00 PM</option>
+              <option>7:00 PM</option>
+              <option>8:00 PM</option>
+              <option>9:00 PM</option>
+              <option>10:00 PM</option>
+              <option>11:00 PM</option>
+            </select>
+          </div>
+          <div id="party-size" className="dropdown">
+            <select id="time-container" className="dropdown">
+              <option>1 person</option>
+              <option>2 people</option>
+              <option>3 people</option>
             </select>
           </div>
         </div>
-        <div id="time-container" className="dropdown">
-          <select>
-            <option>11:00 AM</option>
-            <option>12:00 PM</option>
-            <option>1:00 PM</option>
-            <option>2:00 PM</option>
-            <option>3:00 PM</option>
-            <option>4:00 PM</option>
-            <option>5:00 PM</option>
-            <option>6:00 PM</option>
-            <option>7:00 PM</option>
-            <option>8:00 PM</option>
-            <option>9:00 PM</option>
-            <option>10:00 PM</option>
-            <option>11:00 PM</option>
-          </select>
-        </div>
-        <div id="party-size" className="dropdown">
-          <select>
-            <option>1 person</option>
-            <option>2 people</option>
-            <option>3 people</option>
-          </select>
+        <div>
+          <input
+            id="head-search-bar"
+            type="text"
+            className="dropdown"
+            placeholder="Location, Restaurant, Cuisine"
+          />
         </div>
         <div>
-          <input id="head-search-bar" type="text" className="dropdown" placeholder="Location, Restaurant, Cuisine" />
-        </div>
-        <div >
-          <button id="cont-button" className="dropdown">Continue</button>
+          <button id="cont-button" className="dropdown">
+            Let's Go
+          </button>
         </div>
       </div>
     </header>
