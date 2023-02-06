a985dea896b4a64b7867edfd1e5141ccf7481602
Anthony_Chiodi <adc8wlc@gmail.com>
2023-02-06 14:59:35 -0500

    installing faker

--- Gemfile
+++ Gemfile
@@ -54,4 +54,6 @@ group :development do
   gem "pry-rails"
   gem "better_errors"
   gem "binding_of_caller"
+  # gem "faker"
+  # gem "byebug"
 end
--- bin/render-build.sh
+++ bin/render-build.sh
@@ -4,6 +4,6 @@
 set -o errexit
 
 npm run build
-bundle install --without somerandomstring
+bundle install 
 rails db:migrate
 rails db:seed #if needed
